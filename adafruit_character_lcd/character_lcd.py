05cc3f31b01fbd644093a098803054f59a512a96
sommersoft <sommersoft@gmail.com>
2018-02-24 14:07:46 -0600

    more sphinx fix

--- adafruit_character_lcd/character_lcd.py
+++ adafruit_character_lcd/character_lcd.py
@@ -294,7 +294,7 @@ class Character_LCD(object):
 
     def message(self, text):
         """
-        Write text to display, can include \n for newline
+        Write text to display, can include ``\n`` for newline
 
         :param text: string to display
         """
@@ -312,7 +312,8 @@ class Character_LCD(object):
                 self._write8(ord(char), True)
 
     def create_char(self, location, pattern):
-        """Fill one of the first 8 CGRAM locations with custom characters.
+        """
+        Fill one of the first 8 CGRAM locations with custom characters.
         The location parameter should be between 0 and 7 and pattern should
         provide an array of 8 bytes containing the pattern. E.g. you can easyly
         design your custom character at http://www.quinapalus.com/hd44780udg.html
