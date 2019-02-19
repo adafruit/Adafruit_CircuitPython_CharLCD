d7299d8009e1e571f48dca1d531dd3bfbe55e9ab
Tim-Jackins <timmins.jack595@gmail.com>
2019-02-19 13:20:11 -0500

    Added pcf example

---
+++ examples/charlcd_i2c-simple_mono_simpletest.py
@@ -0,0 +1,52 @@
+"""Simple test for 16x2 character lcd connected to an MCP23008 I2C LCD backpack."""
+import time
+import board
+import busio
+import adafruit_character_lcd.character_lcd_i2c as character_lcd
+
+# Modify this if you have a different sized Character LCD
+lcd_columns = 16
+lcd_rows = 2
+
+# Initialise I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Initialise the lcd class
+lcd = character_lcd.Character_LCD_I2C(i2c, 16, 2, usingPCF=True, address=0x27)
+
+# Turn backlight on
+lcd.backlight = True
+# Print a two line message
+lcd.message = "Hello\nCircuitPython"
+# Wait 5s
+time.sleep(5)
+lcd.clear()
+# Wait 5s
+time.sleep(5)
+# Display cursor
+lcd.clear()
+lcd.cursor = True
+lcd.message = "Cursor! "
+# Wait 5s
+time.sleep(5)
+# Display blinking cursor
+lcd.clear()
+lcd.blink = True
+lcd.message = "Blinky Cursor!"
+# Wait 5s
+time.sleep(5)
+lcd.blink = False
+lcd.clear()
+# Create message to scroll
+scroll_msg = '<-- Scroll'
+lcd.message = scroll_msg
+# Scroll message to the left
+for i in range(len(scroll_msg)):
+    time.sleep(0.5)
+    lcd.move_left()
+lcd.clear()
+lcd.message = "Going to sleep\nCya later!"
+time.sleep(5)
+# Turn backlight off
+lcd.backlight = False
+time.sleep(2)
